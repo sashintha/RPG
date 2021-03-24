18af6baa2b3cbec3ef0034f95dda9111e34498e2
Sashintha Tennakoon <sashitnha2001@gmail.com>
2021-03-24 04:44:25 -0400

    melee attacking and health bar added

--- Assets/__Scripts/PlayerMovement.cs
+++ Assets/__Scripts/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     Vector2 movement;
 
+
     // Update is called once per frame
     void Update()
     {
@@ -20,12 +21,14 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
 }
 
 
