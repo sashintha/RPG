6fc82358f94c893098c9421555d497f20dafec80
jmutton2 <jmutton2@uwo.ca>
2021-03-23 23:41:09 -0400

    Added teleportation, enemy hit registry

---
+++ Assets/__Scripts/EnemyTouch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Hit detected");
+        }
+    }
+}
