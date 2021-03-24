f76c803bdd508f87b7ffbc7c6b5244310875b07e
Sashintha Tennakoon <34927075+sashintha@users.noreply.github.com>
2021-03-24 01:24:50 -0400

    updated healthbar and player movement

--- Assets/__Scripts/PlayerMovement.cs
+++ Assets/__Scripts/PlayerMovement.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthbar;
 
     public float moveSpeed = 80f;
 
@@ -20,11 +24,28 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthbar.SetHealth(currentHealth);
     }
 }
 
